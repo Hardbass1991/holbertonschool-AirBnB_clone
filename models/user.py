d9bac99e68a79665b7b484dc0d03befd758d193c
Hardbass1991 <midailsorridente@gmail.com>
2022-10-15 19:59:31 -0700

    corrected previous rearrangement of attribute enuciation

--- models/user.py
+++ models/user.py
@@ -3,10 +3,10 @@ from models.base_model import BaseModel
 from models import storage
 
 class User(BaseModel):
-    self.email = ""
-    self.password = ""
-    self.first_name = ""
-    self.last_name = ""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
