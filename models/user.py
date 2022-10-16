0cea670fa206a1312037fc379b0a555133867c0f
Hardbass1991 <midailsorridente@gmail.com>
2022-10-15 19:58:00 -0700

    changed order of definition of attributes

--- models/user.py
+++ models/user.py
@@ -3,13 +3,13 @@ from models.base_model import BaseModel
 from models import storage
 
 class User(BaseModel):
+    self.email = ""
+    self.password = ""
+    self.first_name = ""
+    self.last_name = ""
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        if not kwargs:
-            self.email = ""
-            self.password = ""
-            self.first_name = ""
-            self.last_name = ""
 
     def save(self):
         super().save()
